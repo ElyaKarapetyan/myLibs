d7f1fc08d66fd1840cc6a5b3ced58e446382b2ac
Elya Karapetyan <elya@instigatemobile.com>
2020-01-22 14:53:59 +0400

    first commit

---
+++ my-app/src/main/java/com/mycompany/app/Print.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Print
+{
+    public static void sayHello()
+    {
+        System.out.println( "Hello!!!111!" );
+    }
+}
+
